63573e0af8e42735ff59de7f48cf2f116a59e04e
Cristian Morataya <cris.morataya@gmail.com>
2020-04-09 10:50:06 -0600

    Adjust app to rgs2.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const App = ({title}) => (
+        <div className="header">{title}</div>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App title="The Github Cards App"/>,
   document.getElementById('root')
 );
 
